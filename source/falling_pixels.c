4a97fa273bafee3f08eda12da3842e970fbe7f10
Stefan Eng <eng_steff@hotmail.com>
2016-04-03 20:14:49 +0200

    Load GLFW context.

--- source/falling_pixels.c
+++ source/falling_pixels.c
@@ -1,6 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <GLFW/glfw3.h>
+
+#define UNUSED(x) (void)x
+
+void error(char* message) {
+    fprintf(stderr, "ERROR: %s, aborting.\n", message);
+    exit(EXIT_FAILURE);
+}
 
 int main(void) {
-    printf("Hello World!\n");
+
+    GLFWwindow* window;
+
+
+    if (!glfwInit()) {
+        error("Could not load GLFW library");
+    }
+
+
+    UNUSED(window);
 }
