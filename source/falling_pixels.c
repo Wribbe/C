1b02d5d4a04840b3dce60b6bb2b5e050435f72b5
Stefan Eng <eng_steff@hotmail.com>
2016-04-10 11:40:50 +0200

    Clean up falling_pixels.c source.

--- source/falling_pixels.c
+++ source/falling_pixels.c
@@ -1,23 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <GLFW/glfw3.h>
+
+#include "minunit.h"
+#include "falling_pixels.h"
 
 #define UNUSED(x) (void)x
 
-void error(char* message) {
+
+void error(char * message) {
     fprintf(stderr, "ERROR: %s, aborting.\n", message);
     exit(EXIT_FAILURE);
 }
 
+
 int main(void) {
 
-    GLFWwindow* window;
-
+    GLFWwindow * window;
 
     if (!glfwInit()) {
         error("Could not load GLFW library");
     }
-
 
     UNUSED(window);
 }
