82dbd224d499f723b68fcc6505a261b75b104270
Stefan Eng <eng_steff@hotmail.com>
2016-04-14 09:27:07 +0200

    Add comments to dict_put in header file.

--- include/utility_functions.h
+++ include/utility_functions.h
@@ -7,6 +7,12 @@ typedef struct hash_element hash_element;
 
 int dict_define(hash_element ** array_head, size_t size);
 char * dict_get(char * key, hash_element * hash_array);
+
+/* Used to put new key - value pairs into a defined hash_array/dict.
+ * Return values:
+ *   0: everything went fine.
+ *  -1: could not allocate the necessary memory.
+ */
 int dict_put(char * key, char * value, hash_element * hash_array);
 
 #endif
