2fa28b3c72f69238c241e078ea288408c98e0fb7
Stefan Eng <eng_steff@hotmail.com>
2016-04-14 15:35:49 +0200

    Use pointers to pointers to make the dict_put code less verbose.

--- source/utils/utils_dict.c
+++ source/utils/utils_dict.c
@@ -59,47 +59,63 @@ int dict_put(char * key, char * value, hash_element * hash_array) {
 
     hash_element * current_element_pointer;
 
+    char ** current_key = NULL;
+    char ** current_value = NULL;
+    hash_element ** current_next = NULL;
+
     current_element_pointer = &(hash_array[hash_value]);
     while(current_element_pointer != NULL) {
         /* Didn't find the key. */
-        if (current_element_pointer->value == NULL) {
+        current_value = &current_element_pointer->value;
+        current_key = &(current_element_pointer->key);
+        if (*current_value == NULL) {
+
             /* Allocate memory for new key, check for null, copy string data,
              * and ensure null termination. */
-            current_element_pointer->key = malloc(sizeof(char)*length_key+1);
-            if (!current_element_pointer->key) {
+
+            *current_key = malloc(sizeof(char)*length_key+1);
+            if (!*current_key) {
                 return -1;
             }
-            memcpy(current_element_pointer->key, key, length_key);
-            (current_element_pointer->key)[length_key] = '\0';
+            memcpy(*current_key, key, length_key);
+            (*current_key)[length_key] = '\0';
 
             /* Allocate memory for new value, check for null, copy string data,
              * and ensure null termination. */
-            current_element_pointer->value = malloc(sizeof(char)*length_value+1);
-            if (!current_element_pointer->value) {
+
+            current_value = &(current_element_pointer->value);
+            *current_value = malloc(sizeof(char)*length_value+1);
+            if (!*current_value) {
                 return -1;
             }
-            memcpy(current_element_pointer->value, value, length_value);
-            current_element_pointer->value[length_value] = '\0';
+            memcpy(*current_value, value, length_value);
+            (*current_value)[length_value] = '\0';
 
             /* Create new last element. */
-            current_element_pointer->next = malloc(sizeof(hash_element));
-            if (!current_element_pointer->next) {
+            current_next = &(current_element_pointer->next);
+            *current_next = malloc(sizeof(hash_element));
+            if (!*current_next) {
                 return -1;
             }
             /* Set new struct elements to NULL. */
-            current_element_pointer->next->next = NULL;
-            current_element_pointer->next->key = NULL;
-            current_element_pointer->next->value = NULL;
+            hash_element * next_hash = *current_next;
+            next_hash->next = NULL;
+            next_hash->key = NULL;
+            next_hash->value = NULL;
 
             printf("hash: %d for %s returns value == NULL.\n", hash_value, key);
             return 0;
         /* Check if keys match. */
-        } else if (strcmp(current_element_pointer->key, key) == 0) {
+        } else if (strcmp(*current_key, key) == 0) {
             /* Replace the current value with the new value. */
+            *current_value = realloc(*current_value, length_value);
+            *current_value = memcpy(*current_value, value, length_value);
+            (*current_value)[length_value] = '\0';
+            return 0;
         }
 
         /* Continue with next element in linked list. */
-        current_element_pointer = current_element_pointer->next;
+        current_element_pointer = *current_next;
     }
     return 0;
 }
