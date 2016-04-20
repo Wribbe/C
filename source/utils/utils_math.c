43134388a1bc8fdeff7a11d6866e71282f1c5b9d
Stefan Eng <eng_steff@hotmail.com>
2016-04-20 19:45:12 +0200

    Break out matrix iteration into iter_3 and iter_2.

--- source/utils/utils_math.c
+++ source/utils/utils_math.c
@@ -22,7 +22,7 @@ float op_mul(float a, float b) {
     return a * b;
 }
 
-void mat_mul(float * res, float * A, float * B, size_t mat_size, float (*op)(float a, float b)) {
+void mat_iter_3(float * res, float * A, float * B, size_t mat_size, float (*op)(float a, float b)) {
 
     size_t res_index, A_index, B_index;
 
@@ -45,15 +45,37 @@ void mat_mul(float * res, float * A, float * B, size_t mat_size, float (*op)(flo
 }
 
 void mat4_mul(mat4 res, mat4 A, mat4 B) {
-    mat_mul(&res[0][0], &A[0][0], &B[0][0], 4, &op_mul);
+    mat_iter_3(&res[0][0], &A[0][0], &B[0][0], 4, &op_mul);
 }
 
 void mat3_mul(mat3 res, mat3 A, mat3 B) {
-    mat_mul(&res[0][0], &A[0][0], &B[0][0], 3, &op_mul);
+    mat_iter_3(&res[0][0], &A[0][0], &B[0][0], 3, &op_mul);
 }
 
 void mat2_mul(mat2 res, mat2 A, mat2 B) {
-    mat_mul(&res[0][0], &A[0][0], &B[0][0], 2, &op_mul);
+    mat_iter_3(&res[0][0], &A[0][0], &B[0][0], 2, &op_mul);
+}
+
+void mat_iter_2(float * ret, float * A, float * B, size_t mat_size, float (*op)(float a, float b)) {
+    size_t index;
+    for(size_t i = 0; i<mat_size; i++) {
+        for(size_t j = 0; j<mat_size; j++) {
+            index = mat_index(i, j, mat_size);
+            ret[index] =  op(A[index], B[index]);
+        }
+    }
+}
+
+void mat4_add(mat4 res, mat4 A, mat4 B) {
+    mat_iter_2(&res[0][0], &A[0][0], &B[0][0], 4, &op_add);
+}
+
+void mat3_add(mat3 res, mat3 A, mat3 B) {
+    mat_iter_2(&res[0][0], &A[0][0], &B[0][0], 3, &op_add);
+}
+
+void mat2_add(mat2 res, mat2 A, mat2 B) {
+    mat_iter_2(&res[0][0], &A[0][0], &B[0][0], 2, &op_add);
 }
 
 bool mat_cmp(float * A, float * B, size_t mat_size) {
