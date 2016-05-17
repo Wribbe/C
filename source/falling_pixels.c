84dc42c9e5c230e28309e47c44c7f967634d94c2
Stefan Eng <eng_steff@hotmail.com>
2016-05-17 22:45:24 +0200

    Change to dynamic draw.

--- source/falling_pixels.c
+++ source/falling_pixels.c
@@ -276,7 +276,7 @@ int main(void) {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
